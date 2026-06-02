98dac917d2256e99684115c9e85b37d1479ea4af
1085680176 <1085680176@qq.com>
2026-06-02 08:41:04 +0800

    Update diy-part1.sh
    Sync ophub on Mar 14, 2026

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #========================================================================================================================
 # https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Description: Automatically Build OpenWrt
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
 # Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
 #========================================================================================================================
@@ -11,4 +11,3 @@
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
-
